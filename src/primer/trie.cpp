64d2f84915f2ecc1e4000dd0febdd10b6e8d17b1
aakarshan-raj <aakarshan7raj@gmail.com>
2024-06-14 21:07:59 +0530

    trie node GET test

---
+++ src/primer/trie.cpp
@@ -0,0 +1,78 @@
+#include "primer/trie.h"
+#include <string_view>
+#include "common/exception.h"
+
+namespace bustub {
+
+template <class T>
+auto Trie::Get(std::string_view key) const -> const T * {
+  if (root_ == nullptr) {
+    return nullptr;
+  }
+  auto temp_node = root_;
+  for (auto &c : key) {
+    if (!(temp_node->HasChild(c))) {
+      return nullptr;
+    }
+    temp_node = temp_node->GetChildNode(c);
+    if (temp_node == nullptr) {
+      return nullptr;
+    }
+  }
+  if (key.empty()) {
+    temp_node = root_->GetChildNode(*key.begin());
+  }
+  if (!temp_node->is_value_node_) {
+    return nullptr;
+  }
+
+  auto node_value_pointer = std::dynamic_pointer_cast<const TrieNodeWithValue<T>>(temp_node);
+  if ((!node_value_pointer)) {
+    return nullptr;
+  }
+  return node_value_pointer->value_.get();
+}
+
+template <class T>
+auto Trie::Put(std::string_view key, T value) const -> Trie {
+  // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
+  throw NotImplementedException("Trie::Put is not implemented.");
+
+  // You should walk through the trie and create new nodes if necessary. If the node corresponding to the key already
+  // exists, you should create a new `TrieNodeWithValue`.
+}
+
+auto Trie::Remove(std::string_view key) const -> Trie {
+  throw NotImplementedException("Trie::Remove is not implemented.");
+
+  // You should walk through the trie and remove nodes if necessary. If the node doesn't contain a value any more,
+  // you should convert it to `TrieNode`. If a node doesn't have children any more, you should remove it.
+}
+
+// Below are explicit instantiation of template functions.
+//
+// Generally people would write the implementation of template classes and functions in the header file. However, we
+// separate the implementation into a .cpp file to make things clearer. In order to make the compiler know the
+// implementation of the template functions, we need to explicitly instantiate them here, so that they can be picked up
+// by the linker.
+
+template auto Trie::Put(std::string_view key, uint32_t value) const -> Trie;
+template auto Trie::Get(std::string_view key) const -> const uint32_t *;
+
+template auto Trie::Put(std::string_view key, uint64_t value) const -> Trie;
+template auto Trie::Get(std::string_view key) const -> const uint64_t *;
+
+template auto Trie::Put(std::string_view key, std::string value) const -> Trie;
+template auto Trie::Get(std::string_view key) const -> const std::string *;
+
+// If your solution cannot compile for non-copy tests, you can remove the below lines to get partial score.
+
+using Integer = std::unique_ptr<uint32_t>;
+
+template auto Trie::Put(std::string_view key, Integer value) const -> Trie;
+template auto Trie::Get(std::string_view key) const -> const Integer *;
+
+template auto Trie::Put(std::string_view key, MoveBlocked value) const -> Trie;
+template auto Trie::Get(std::string_view key) const -> const MoveBlocked *;
+
+}  // namespace bustub
